7a2aebb52f9dadcce1dd2bc122a892a7a906819e
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-04-19 23:25:12 -0600

    intro to flutter course
    first apps

---
+++ FirebaseMVC/FirebaseMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FirebaseMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {    
