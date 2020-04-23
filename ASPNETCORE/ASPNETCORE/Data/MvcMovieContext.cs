2b0dd71aa1accd7dc8b66f7b201173539bbf137e
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-04-22 22:58:56 -0600

    agregando nuevo campo a la base de datos

---
+++ ASPNETCORE/ASPNETCORE/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCORE.Models;
+
+namespace ASPNETCORE.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ ASPNETCORE/ASPNETCORE/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASPNETCORE.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]        
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+    }
+}
