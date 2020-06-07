69d72dfba880f150a5dd2dde10eff79bf5db180d
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-06-06 23:46:31 -0600

    adding login components

--- testAngular/testAngular/ClientApp/src/app/app.module.ts
+++ testAngular/testAngular/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
+import { SecretComponent } from './secret/secret.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    LoginComponent
+    LoginComponent,
+    SecretComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
--- testAngular/testAngular/ClientApp/src/app/secret/secret.component.spec.ts
+++ testAngular/testAngular/ClientApp/src/app/secret/secret.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { SecretComponent } from './secret.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('SecretComponent', () => {
+  let component: SecretComponent;
+  let fixture: ComponentFixture<SecretComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ SecretComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(SecretComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
